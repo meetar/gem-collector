ceff789c0d2dd20e589051fe51e9802edcfb5189
Peter Richardson <pxrich@gmail.com>
2023-09-23 19:02:47 -0700

    Updates

--- archive/Fetcher.js
+++
@@ -1,8 +0,0 @@
-export default function fetcher(url) {
-  return fetch(url)
-  .then(response => response.text())
-  .then(data => {
-    return(data);
-  })
-  .catch(error => console.error('Error fetching:', error));
-};
