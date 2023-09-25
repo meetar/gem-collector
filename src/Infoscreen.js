cfd49c5146fa06f21e24d72ba4ff350eaa23cf98
Peter Richardson <pxrich@gmail.com>
2023-09-24 20:14:46 -0700

    about

--- src/Infoscreen.js
+++ src/Infoscreen.js
@@ -3,15 +3,13 @@ export const Infoscreen = ({nightModeClass, toggleInfo}) => {
     <div id="infoscreen" className={`${nightModeClass}`} onClick={toggleInfo}><div id="infocontent">
       
       <h3>The Gem Collector</h3>
-      <p>~ by Peter and Juna ~</p>
+      <p>~ by <a href="http://github.com/meetar">Peter and Juna</a> ~</p>
 
-      <p>Gems were made in <a href="https://threejs.org/">three.js</a> using <a href="https://github.com/pmndrs/react-three-fiber">react-three-fiber</a>, <a href='https://github.com/pmndrs/drei'>drei</a>, and <a href="http://blender.org">Blender</a>.</p>
-
-      <p>Other tools used include <a href="https://github.com/pmndrs/leva">Leva</a>, <a href="https://www.pixilart.com/">pixilart</a>, and <a href="https://www.typeitjs.com/">TypeIt</a>.</p>
+      <p>Made with <a href="https://threejs.org/">three.js</a>, <a href="https://github.com/pmndrs/react-three-fiber">react-three-fiber</a>, <a href='https://github.com/pmndrs/drei'>drei</a>, <a href="http://blender.org">Blender</a>, <a href="https://github.com/pmndrs/leva">Leva</a>, <a href="https://www.pixilart.com/">pixilart</a>, and <a href="https://www.typeitjs.com/">TypeIt</a>.</p>
 
       <p>~ Thank you to the <a href="https://pmnd.rs/">Poimandres</a> open source developer collective ~</p>
 
-      <p>💎</p>
+      <p>Sept. 2023<br/>💎</p>
       </div></div>
     )
   }
