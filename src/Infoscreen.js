8dc30aadc924a639a86dc6cdc9c21a8944ba0e61
Peter Richardson <pxrich@gmail.com>
2023-09-24 21:19:08 -0700

    add repo link

--- src/Infoscreen.js
+++ src/Infoscreen.js
@@ -3,7 +3,7 @@ export const Infoscreen = ({nightModeClass, toggleInfo}) => {
     <div id="infoscreen" className={`${nightModeClass}`} onClick={toggleInfo}><div id="infocontent">
       
       <h3>The Gem Collector</h3>
-      <p>~ by <a href="http://github.com/meetar">Peter and Juna</a> ~</p>
+      <p>~ by <a href="http://github.com/meetar/gem-collector">Peter and Juna</a> ~</p>
 
       <p>Made with <a href="https://threejs.org/">three.js</a>, <a href="https://github.com/pmndrs/react-three-fiber">react-three-fiber</a>, <a href='https://github.com/pmndrs/drei'>drei</a>, <a href="http://blender.org">Blender</a>, <a href="https://github.com/pmndrs/leva">Leva</a>, <a href="https://www.pixilart.com/">pixilart</a>, and <a href="https://www.typeitjs.com/">TypeIt</a>.</p>
 
